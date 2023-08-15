e43b035754656d38973af1298ba5be1f20292bf1
leonardo oliveira <azideiamano20209@gmail.com>
2023-08-15 10:16:00 -0300

    comecei a fazer uma array de uma loja virtual

---
+++ up.js
@@ -0,0 +1,19 @@
+class produto{
+
+    constructor() {
+        this.id = 0;
+        this.nomeProduto = '';
+        this.valor =0;
+    }
+    adicionar() {
+        alert("vamos adicionar um produto!");
+
+    }
+    excluir(){
+
+
+
+        alert('item deletado!');
+    }
+}
+var produto = new produto();
